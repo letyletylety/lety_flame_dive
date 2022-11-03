ead666e4b1b44d84507bccdfcc30b12fb7dee410
Lety <taej0127@gmail.com>
2022-11-03 16:11:25 +0900

    add game

---
+++ lib/game.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class MyGame extends Game {
+  @override
+  void render(Canvas canvas) {}
+
+  @override
+  void update(double dt) {}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+
+import 'game.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Game _game;
+
+  @override
+  void initState() {
+    super.initState();
+    _game = MyGame();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,6 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: GameWidget(game: _game),
     );
   }
 }
